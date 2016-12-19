d598af0dfa93586ec7c79b774c20b827a2e27cbd
Cy Kong <cykong@Regie-Kongs-MacBook-Pro.local>
2016-12-19 15:58:35 -0800

    passes all pretests for BlinkyDancer

--- src/dancer.js
+++ src/dancer.js
@@ -4,16 +4,18 @@ var makeDancer = function(top, left, timeBetweenSteps) {
   this.left = left;
   this.timeBetweenSteps = timeBetweenSteps;
   this.$node = $('<span class="dancer"></span>');
+  this.step();
+  this.setPosition(top, left)
 };
 
 makeDancer.prototype.step = function () {
-  setTimeout(this.step, this.timeBetweenSteps);
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 
 makeDancer.prototype.setPosition = function (top, left) {
   var styleSettings = {
   top: top,
-  left: left,
-  this.$node.css(styleSettings);
-  }  
+  left: left
+  }
+  this.$node.css(styleSettings);  
 };
